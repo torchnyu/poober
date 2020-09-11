d1ef589be0e3a8d4ac8be5d621e47ef703e0bdbe
nysteo <teonys@nyu.edu>
2020-09-10 23:59:34 -0400

    setting up frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {Typography, Grid, Fade, makeStyles} from '@material-ui/core';
 
-function App() {
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    //CSS GOES HERE
+  }
+}));
+
+const App = (props) => {
+  const classes = useStyles();
+  const apiLink = 'your api link here';
+  const [userLat, setUserLat] = useState(null);
+  const [userLon, setUserLon] = useState(null);
+  const getUserPosition = () => { // example to show how to retrieve current user position.
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setUserLat(position.coords.latitude);
+      setUserLon(position.coords.longitude);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Typography> Hello World </Typography>
+  )
+  
 }
-
 export default App;
+
