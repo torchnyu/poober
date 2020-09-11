2b467a313a049cca75adb09e5287dd20c3664712
Matthew Apuya <m.apuya19@gmail.com>
2020-09-11 00:20:51 -0400

    format with Prettier

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import {Typography, Grid, Fade, makeStyles} from '@material-ui/core';
-
+import React, { useState, useEffect } from "react";
+import { Typography, Grid, Fade, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     //CSS GOES HERE
-  }
+  },
 }));
 
 const App = (props) => {
   const classes = useStyles();
-  const apiLink = 'your api link here';
+  const apiLink = "your api link here";
   const [userLat, setUserLat] = useState(null);
   const [userLon, setUserLon] = useState(null);
-  const getUserPosition = () => { // example to show how to retrieve current user position.
-    navigator.geolocation.getCurrentPosition(function(position) {
+  const getUserPosition = () => {
+    // example to show how to retrieve current user position.
+    navigator.geolocation.getCurrentPosition(function (position) {
       setUserLat(position.coords.latitude);
       setUserLon(position.coords.longitude);
     });
-  }
-  return (
-    <Typography> Hello World </Typography>
-  )
-  
-}
+  };
+  return <Typography> Hello World </Typography>;
+};
 export default App;
-
